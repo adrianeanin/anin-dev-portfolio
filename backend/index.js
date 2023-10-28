76f69d3742b0b6658be333fe89349575b96d1fd4
adriananin <113365066+adriananin@users.noreply.github.com>
2023-10-28 18:18:16 +0000

    add contact form backend

---
+++ backend/index.js
@@ -0,0 +1,70 @@
+require("dotenv").config();
+const express = require("express");
+const nodemailer = require("nodemailer");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+const PORT = process.env.PORT;
+
+const transport = {
+  service: "outlook",
+  auth: {
+    user: process.env.EMAIL,
+    pass: process.env.PASSWORD,
+  },
+};
+
+const transporter = nodemailer.createTransport(transport);
+
+transporter.verify((error, success) => {
+  if (error) console.log(error);
+  else {
+    console.log("Server is ready to take messages");
+  }
+});
+
+app.post("/email", (req, res) => {
+  const { name, email, message } = req.body;
+
+  const mailOptions = {
+    from: `${name} <${process.env.EMAIL}>`,
+    to: process.env.EMAIL,
+    subject: `New Portfolio Message from ${name}`,
+    text: `Name: ${name}\nEmail: ${email}\nMessage: ${message}`,
+  };
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.error(error);
+      return res.status(500).send(`Error sending email: ${error.toString()}`);
+    }
+    res.redirect("/thanks");
+  });
+});
+
+app.get("/", (req, res) => {
+  res.send("<h1>my endpoint</h1>");
+});
+
+app.get("/thanks", (req, res) => {
+  const options = {
+    root: __dirname,
+    headers: {
+      "Content-Type": "text/html",
+    },
+  };
+  res.status(200).sendFile("thanks.html", options, (err) => {
+    if (err) {
+      console.error(err);
+      res.status(err.status).end();
+    }
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
--- frontend/public/theme-toggle.js
+++ frontend/public/theme-toggle.js
@@ -1,7 +1,6 @@
 const storageKey = "theme-preference";
 
 const onClick = () => {
-  // flip current value
   theme.value = theme.value === "light" ? "dark" : "light";
 
   setPreference();
@@ -36,14 +35,10 @@ const theme = {
 reflectPreference();
 
 window.onload = () => {
-  // set on load so screen readers can see latest value on the button
   reflectPreference();
-
-  // now this script can find and listen for clicks on the control
   document.querySelector("#theme-toggle").addEventListener("click", onClick);
 };
 
-// sync with system changes
 window
   .matchMedia("(prefers-color-scheme: dark)")
   .addEventListener("change", ({ matches: isDark }) => {
